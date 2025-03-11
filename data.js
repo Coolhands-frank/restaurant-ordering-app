68dca00fc8a44857f421af8e90256b43f2f35864
Elendu Franklin <elendufranklin66@gmail.com>
2025-03-11 08:00:16 +0100

    ui update

--- data.js
+++ data.js
@@ -1,14 +1,14 @@
 export const menuArray = [
     {
         name: "Pizza",
-        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        ingredients: ["pepperoni, mushrom"],
         id: 0,
         price: 14,
         emoji: "🍕"
     },
     {
         name: "Hamburger",
-        ingredients: ["beef", "cheese", "lettuce"],
+        ingredients: ["beef, cheese, lettuce"],
         price: 12,
         emoji: "🍔",
         id: 1
